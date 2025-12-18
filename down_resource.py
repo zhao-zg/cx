355980daabe1043f91cfae7f422ee066fb5d05ee
zhao-zg <787854091@qq.com>
2025-12-18 14:04:20 +0800

    修复使用无头

--- down_resource.py
+++ down_resource.py
@@ -451,7 +451,7 @@ def playwright_downloads(all_docs: List[Dict[str, Any]]) -> None:
         docs_by_resource[doc['resource_id']].append(doc)
     
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         
